100f4243878e9325e9f57e3d2f38b803137694ee
hara6 <paranalu6@gmail.com>
2025-05-31 12:27:10 +0900

    update: dbの接続を追加

--- backend/api/app/userLogin.py
+++ backend/api/app/userLogin.py
@@ -1,14 +1,22 @@
 from pydantic import BaseModel
+import sqlite3
+
+dbname = ''
 
 class LoginRequest(BaseModel):
     username: str
     password: str
 
-def authenticate_user(req):
-    username = req.username
-    password = req.password
+def authenticate_user(req: LoginRequest):
 
-    if username == "admin" and password == "secret":
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users WHERE username=? AND password=?", (req.username, req.password))
+    user = cur.fetchone() #検索結果のうち、最初の1件だけを取得
+    conn.close()
+
+    if user:
         return {"message": "Login successful"}
     else:
         return {"message": "Invalid credentials"}
